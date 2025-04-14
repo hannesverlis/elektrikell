9350784eee06eadfc56301ba1472390512993d76
Hannes Verlis <61347431+hannesverlis@users.noreply.github.com>
2025-04-14 12:06:27 +0300

    Commit

--- src/main/java/ee/elering/hind/service/PriceFetcherService.java
+++ src/main/java/ee/elering/hind/service/PriceFetcherService.java
@@ -32,26 +32,19 @@ public class PriceFetcherService {
         JSONObject dataItemFI = jsonResponseFI.getJSONArray("data").getJSONObject(0);
 
         double priceFI = dataItemFI.getDouble("price");
-        long timestampSecondsFI = dataItemFI.getLong("timestamp");
-
 
         // EE
-        // ✅ Teisendame Unix timestamp'i õigesse Eesti ajavööndisse
-        Instant instantEE = Instant.ofEpochSecond(timestampSecondsEE);
-        ZonedDateTime dateTimeEE = instantEE.atZone(ZoneId.of("Europe/Tallinn"));
-        // ✅ Vormindame kuupäeva ja kellaaja: nt 14.04.2025 01:00
+        // Convert Unix timestamp to the correct Estonian time zone.  Teisendame Unix timestamp'i õigesse Eesti ajavööndisse
+        Instant instant = Instant.ofEpochSecond(timestampSecondsEE);
+        ZonedDateTime dateTime = instant.atZone(ZoneId.of("Europe/Tallinn"));
+        // We format the date and time:  14.04.2025 01:00
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");//
-        String formattedTimeEE = dateTimeEE.format(formatter);
-
-        //FI
-        Instant instantFI = Instant.ofEpochSecond(timestampSecondsFI);
-        ZonedDateTime dateTimeFI = instantFI.atZone(ZoneId.of("Europe/Helsinki"));
-        String formattedTimeFI = dateTimeFI.format(formatter);
+        String formattedTime = dateTime.format(formatter);
 
         double priceDiff = priceEE - priceFI;
 
-        // ✅ Roheline tekst uue reaga HTML-na (sobib brauseris)
-        return "<span style='color:green;'>This method gets latest available NordPool EE day-ahead price<br><br>Time: " + formattedTimeEE + "<br><br>Estonia EE price: " + priceEE + " EUR/MWh<br><br>Finland FI price: " + priceFI + "EUR/MWh <br><br> <span style='color:red;'> Price difference (EE-FI) " + priceDiff + " EUR</span>"+ "</span";
+        //  HTML format
+        return "<span style='color:green;'>This method gets latest available NordPool EE day-ahead price<br><br>Time: " + formattedTime + "<br><br>Estonia EE price: " + priceEE + " EUR/MWh<br><br>Finland FI price: " + priceFI + "EUR/MWh <br><br> <span style='color:red;'> Price difference (EE-FI) " + priceDiff + " EUR</span>" + "</span";
     }
 
 }

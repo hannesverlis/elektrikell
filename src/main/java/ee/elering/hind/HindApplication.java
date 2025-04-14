48cc539e8e59b4f04d277b36eaa56c072d683d42
Hannes Verlis <61347431+hannesverlis@users.noreply.github.com>
2025-04-14 11:06:14 +0300

    Commit

---
+++ src/main/java/ee/elering/hind/HindApplication.java
@@ -0,0 +1,12 @@
+package ee.elering.hind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HindApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HindApplication.class, args);
+	}
+}
